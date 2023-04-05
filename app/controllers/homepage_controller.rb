b5238933cc103765f05f15dc6a9f667f2cf74c14
Moskvin_Dmitry <moskvin_03@mail.ru>
2023-04-05 15:32:21 +0600

    add filter

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,14 @@
 class HomepageController < ApplicationController
+  before_filter :authenticate_user!, except => [:show, :index]
   def index
+    # do something
+  end
+
+  def show
+    # do something
+  end
+
+  def create
+    # do something
   end
 end
