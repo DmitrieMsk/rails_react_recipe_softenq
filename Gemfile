5269416b0a866923fada2e8e9ad6354623c22dc4
Moskvin_Dmitry <moskvin_03@mail.ru>
2023-04-05 14:17:43 +0600

    generate controller pers profile and add config gemfile 3.1 - 3.3

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem "sqlite3", "~> 1.4"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
+#добавил по гайду с хабра шаг 1
+gem 'devise'
+
 # Bundle and transpile JavaScript [https://github.com/rails/jsbundling-rails]
 gem "jsbundling-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'persons/profile'
+  #не менял по шагу 3.2
   root 'homepage#index'
+  #добавил строку по шагу 3.2
+  get 'persons/profile', as: 'user_root'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
