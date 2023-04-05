4623e61df0dc0507754d943fed101c350fede573
Moskvin_Dmitry <moskvin_03@mail.ru>
2023-04-05 15:35:57 +0600

    add before_action in homepage controller.rb file

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,5 @@
 class HomepageController < ApplicationController
-  before_filter :authenticate_user!, except => [:show, :index]
+  before_action :authenticate_user!, except => [:show, :index]
   def index
     # do something
   end
